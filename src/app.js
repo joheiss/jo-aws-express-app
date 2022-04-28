7996a97ad371243847a76c963d1f679cb9e9c528
Josef Heiss <josef.heiss@jovisco.de>
2022-04-28 09:54:19 +0200

    JS instead  of TS

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hi Josef, this is just Express with JS");
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`[SERVER-LOG] Server started at port ${PORT}`);
+});
+
+module.exports = app;
