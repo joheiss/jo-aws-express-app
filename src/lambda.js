5fc330a18ede688c5f1cf83efb7a8d52864277d8
Josef Heiss <josef.heiss@jovisco.de>
2022-04-28 10:21:44 +0200

    corrected error in JS

--- src/lambda.js
+++ src/lambda.js
@@ -1,3 +1,3 @@
 const serverlessExpress = require("@vendia/serverless-express");
 const app = require("./app");
-exports.handler = serverless({ app });
+exports.handler = serverlessExpress({ app });
