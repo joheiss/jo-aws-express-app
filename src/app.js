409a4f44b5513cc7c62a16d6c55a9b334ac3a830
Josef Heiss <josef.heiss@jovisco.de>
2022-04-29 15:19:31 +0200

    break code to test rollback

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hi Josef, this is just Express with JS");
+    res.sendStatus(500);
+    // res.send("Hi Josef, this is just Express with JS");
 });
 
 const PORT = 8080;
